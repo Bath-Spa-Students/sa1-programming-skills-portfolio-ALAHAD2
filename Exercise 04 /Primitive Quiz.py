5ff1f24526dd5c12550a0c1287c79200c8e67e8e
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 14:34:46 +0400

    Primitive Quiz.py

---
+++ Exercise 04 /Primitive Quiz.py
@@ -0,0 +1,9 @@
+#  The work of question is capital city of france
+Answer =int(input("what is the capital city of france ?"))
+# the answer of question
+if Answer == "Paris":
+   print("correct answer of the capital city of france is paris")
+else:
+   print("Sorry for incorrect, the capital city of france is paris")
+
+

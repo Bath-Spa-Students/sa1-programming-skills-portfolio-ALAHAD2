7e96a1b172b4815b955aa98297da48d76b0b2f5d
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 14:45:13 +0400

    Some Counting.py

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,24 @@
+#STEP1:counts up from 0 to 50 in increments of 1.
+print("counts up from 0 to 50 in increments of 1.")
+for i in range (51):
+    print(i)
+
+#STEP2:counts down from 50 to 0 in decrements of 1
+print("\n counts down from 50 to 0 in decrements of 1.")
+for j in range (51,-1,-1):
+    print(j)
+
+#STEP3:counts up from 30 to 50 in increments of 1.
+print("\ncounts up from 30 to 50 in incrementsof 1. ")
+for k in range(30,51):
+    print(k)
+
+#STEP4:counts down from 50 to 10 in decrements of 2.
+print("\n counts down from 50 to 10 in decrements of 2. ")
+for l in range(50,9,-2):
+    print(l)
+
+#STEP5:counts up from 100 to 200 in increments of 5.
+print("\n counts up from 100 to 200 in increments of 5.")
+for x in range(100,201,5):
+    print(x)

5fafffd94aa0cc7c327704b5a7d08e99b5c49fa3
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:45:06 +0400

    PERFORMING CALCULATIONS THROUGH OPERATOR.py

---
+++ Class Activities/#PERFORMING CALCULATIONS THROUGH OPERATOR.py
@@ -0,0 +1,16 @@
+#PERFORMING CALCULATIONS THROUGH OPERATORS
+print (7+ 8)
+print (10-8)
+print (30 *2)
+print (60/5) # for floating point
+print (70/8)   # for interger
+
+# performing calculation through operation 
+print(10+50)
+print(50- 50)
+print(48 *5)
+print(70/9) # for floating point
+print(65/5)   # for interger
+
+
+#

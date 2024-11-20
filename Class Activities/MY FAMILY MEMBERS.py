62924285b66e94448c424c8b2f919bd4f7de89f5
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:46:08 +0400

    MY FAMILY MEMBERS.py

---
+++ Class Activities/MY FAMILY MEMBERS.py
@@ -0,0 +1,15 @@
+#MY FAMILY MEMBERS
+father_name = input (" enter your father name:")
+print(father_name)
+
+mother_name = input (" enter your mother name:")
+print(mother_name)
+
+brother_name = input(" enter your brother name: ")
+print(brother_name)
+
+age = int(input("enter your brother age:"))
+print(age)
+
+age = float(input("enter your brother age:"))
+print(age)

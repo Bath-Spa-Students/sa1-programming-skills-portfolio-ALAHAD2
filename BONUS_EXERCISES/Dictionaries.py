7797c9b710e1a500c5c0cc993a5284d48f248171
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:34:31 +0400

    Dictionaries.py

---
+++ BONUS_EXERCISES/Dictionaries.py
@@ -0,0 +1,12 @@
+# TO create personal information with a dictionary
+Personal_info ={
+    "first_name": 'Al ahad',
+    "last_name": 'N.R',
+    "age": 20,
+    "city": 'Ras al khaimah'
+}
+# Print each piece of information stored in dictionary
+print("First name:",Personal_info['first_name'] )
+print("Last name:",Personal_info['last_name'])
+print("Age:",Personal_info['age'])
+print("City:",Personal_info['city'])

98e9212b72dcdb3c40bf8d5a556ba206b6a627ee
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 14:39:43 +0400

    variables.py

---
+++ Class Activities/variables.py
@@ -0,0 +1,7 @@
+#To assign message the variable
+Message = "I am Al ahad"
+print(Message)
+
+#To change the value of variable in a new message
+message  = "I am from bath spa university"
+print(message)

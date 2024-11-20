8290fecdae0f076fb480f2d778d5d850c2122269
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 14:38:21 +0400

    workout.py

---
+++ Class Activities/workout.py
@@ -0,0 +1,11 @@
+# Repitition Operator * To repeat data
+
+numbers = [1,2,3,4]
+new_numbers = numbers * 2
+print(new_numbers) 
+
+# Repitition Operator * To repeat data
+
+numbers1 = [5,6,7,8,9]
+new_numbers = numbers1 * 6
+print(new_numbers)

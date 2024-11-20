839c8f3d7ed4f1d08d1b311aa09888011d065c40
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:50:45 +0400

    Dictionary For Loop.py

---
+++ Class Activities/Dictionary For Loop.py
@@ -0,0 +1,23 @@
+# Dictionary For Loop
+THISDICT ={
+      "name":"Al ahad",
+      "father":"Nijam",
+      "mother":"Raseena"
+}
+for a in THISDICT:
+   print(a)
+
+example = {'color' : 'red '  , 
+           'fruit' : 'apple' ,
+           'place' : 'United arab emirates '
+}
+for u in example:
+  print(example[u])
+
+example = {'color' : 'red '  , 
+           'fruit' : 'apple' ,
+           'place' : 'United arab emirates '
+}
+for x in example.values():
+  print(x)
+

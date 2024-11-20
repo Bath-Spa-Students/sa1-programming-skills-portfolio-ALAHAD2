0e35cf89f22bae7d08fea5496dbb141ada47da31
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 14:48:25 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,11 @@
+#This will print "Hello" to the console
+def hello():
+    print("Hello")
+
+#This calls the hello() function
+def main():
+    hello()
+
+#This will run as main function
+if __name__ == "__main__":
+    main()

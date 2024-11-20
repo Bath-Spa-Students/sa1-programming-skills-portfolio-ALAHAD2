34f7c1076698b4c841fc8d9764b3b4496948ae52
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:43:54 +0400

    if elif else.py

---
+++ Class Activities/if elif else.py
@@ -0,0 +1,13 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+  c = 99
+  d = 100
+  if d > c:
+    print("d is greater than c")
+  elif c == d:
+    print("c and d are equal")

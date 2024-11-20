b01fcfb544aec9694f5081c7b600920d9b0920e2
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:30:51 +0400

    Variables (Favorite Number).py

---
+++ BONUS_EXERCISES/Variables (Favorite Number).py
@@ -0,0 +1,5 @@
+#The favorite number of variable
+favorite_number= 10
+#Create a message that reveals in your favorite number
+message= f"My most favorite number is {favorite_number}"
+print(message)

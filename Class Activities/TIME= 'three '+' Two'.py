91a1acbd7fb9a990fb92a03f76d2263fbe5c35d5
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:38:59 +0400

    TIME= 'three '+' Two'.py

---
+++ Class Activities/TIME= 'three '+' Two'.py
@@ -0,0 +1,17 @@
+TIME= 'three '+'  Two'
+print(TIME)
+
+
+print('how are you brother '+
+      'i am fine brother '+
+      'and you brother. ')
+
+
+#Welcome to walt disney
+Name = 'al '+'ahad'
+print(Name)
+
+a="hello "
+b="Welcome"
+c= a + " " + b
+print(c)

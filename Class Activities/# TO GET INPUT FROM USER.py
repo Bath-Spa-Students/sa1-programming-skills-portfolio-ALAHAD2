3ab3d4a5d13efe61461201e0dbb58ae20b3a8147
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:41:52 +0400

    TO GET INPUT FROM USER.py

---
+++ Class Activities/# TO GET INPUT FROM USER.py
@@ -0,0 +1,13 @@
+# TO GET INPUT FROM USER
+first_name = input  ("  enter your name:")
+print(first_name)
+
+
+last_name = input("enter your last name:")
+print(last_name)  
+
+age = int(input ("enter your age:"))
+print(age)
+ 
+age = float(input ("enter your age:"))
+print = (age)

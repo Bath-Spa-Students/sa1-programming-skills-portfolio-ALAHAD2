1279c1ec94f2a3fc65f0c83838351fb25df16daf
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:28:57 +0400

    Strings Concatenation.py

---
+++ BONUS_EXERCISES/Strings Concatenation.py
@@ -0,0 +1,7 @@
+#The value of first variable
+variable1= "My"
+#The value of second variable
+variable2= " name is"
+#The value of third variable
+variable3= "Al ahad"
+print(variable1+variable2+" "+variable3)

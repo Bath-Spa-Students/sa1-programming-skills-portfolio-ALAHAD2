504e25dc61b3f5da0b15f6a0dd92b40cb9b6d680
ALAHAD2 <alahadnr87@gmail.com>
2024-11-20 20:33:34 +0400

    Control Structures.py

---
+++ BONUS_EXERCISES/Control Structures.py
@@ -0,0 +1,12 @@
+# the colour of the alien variable
+alien_colour ='green'
+#If colour of the alien is green
+if alien_colour == 'green':
+    print("the player just earned 5 points")
+
+
+# the colour of the alien variable
+alien_colour ='yellow'
+#If colour of the alien is green
+if alien_colour == 'green':
+    print("the player just earned 5 points")
